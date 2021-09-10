c815aa57e4610ca20a886e7ae1f57c24b6d18e34
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2021-09-10 12:50:05 +0200

    updated details page

--- js/detail.js
+++ js/detail.js
@@ -1,7 +1,5 @@
 
 const pokemonContainer = document.querySelector(".pokemon-details");
-const pokemonHeader = document.querySelector(".pokemon-header");
-const attacksContainer = document.querySelector(".attacksContainer");
 const errorContainer = document.querySelector(".errorContainer");
 const title = document.querySelector("title");
 const queryString = document.location.search;
@@ -21,6 +19,7 @@ async function pokemonInfo() {
         const pokemonDetails = result.data;
         const final = pokemonDetails
         pokemonContainer.innerHTML = "";
+        errorContainer.style.display = "none";
 
         title.innerHTML = `${final.name}`;
         console.log(final.attacks);
@@ -42,54 +41,50 @@ pokemonInfo();
 
 
 function createAttacks() {
-
     for (let i = 0; i < final.attacks.length; i++) {
       if (!final[i].attacks[1]) {
         continue;
       }
     }
-}
+  }
 
 
 function getAttacks(attacks) {
-
     let output = `<div class="cardStyle">
-                  <h4 class="cardHeading">Attacks</h4>`;
+                    <h4 class="cardHeading">Attacks</h4>`;
     for (let i = 0; i < attacks.length; i++) {
       output += `<p>${attacks[i].name}</p>`;
     }
     output += `</div>`;
     return output;
-}
-
+  }
 
 
 function createHtml(final) {
 
-
-    pokemonContainer.innerHTML = `
-                                <img class="pokemon-image" src="${final.images.small}">
-                                <h2 class="pokeName">${final.name}</h2>
-                                <div class="cardDetails">
-                                `;
-                            
-                                pokemonContainer.innerHTML += getAttacks(final.attacks);
-                                
-                            
-                                pokemonContainer.innerHTML += `
-                                                                </div>
-                                                                <div class="cardStyle">
-                                                                    <h4 class="cardHeading">Rarity</h4>
-                                                                    <p>${final.rarity}</p>
-                                                                </div>
-                                                                <div class="cardStyle">
-                                                                    <h4 class="cardHeading">Weaknesses</h4>
-                                                                    <p>Type: ${final.weaknesses[0].type}</p>
-                                                                    <p>Value: ${final.weaknesses[0].value}</p>
-                                                                </div>
-                                                                </div>
-                                                                `;
-}
+    let htmlString = '';
+    htmlString += `
+      <img class="pokemon-image" src="${final.images.small}">
+      <h2 class="pokeName">${final.name}</h2>
+      <div class="cardDetails">`;
+  
+    htmlString += getAttacks(final.attacks);
+  
+    htmlString += `
+        <div class="cardStyle">
+            <h4 class="cardHeading">Rarity</h4>
+            <p>${final.rarity}</p>
+        </div>
+        <div class="cardStyle">
+            <h4 class="cardHeading">Weaknesses</h4>
+            <p>Type: ${final.weaknesses[0].type}</p>
+            <p>Value: ${final.weaknesses[0].value}</p>
+        </div>
+      </div>
+      `;
+  
+    pokemonContainer.innerHTML = htmlString;
+  }
 
 
    
